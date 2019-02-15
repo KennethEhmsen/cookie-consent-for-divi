676a0fd99e5d1665503f0b06535d49efe116cb7e
fahimxyz <hi@fahim.xyz>
2019-02-15 12:32:08 +0600

    Initial commit from Create React App

---
+++ includes/__PluginName.php
@@ -0,0 +1,46 @@
+<?php
+
+class DVPPL_DiviCookieConsent extends DiviExtension {
+
+	/**
+	 * The gettext domain for the extension's translations.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string
+	 */
+	public $gettext_domain = 'dvppl-divi-cookie-consent';
+
+	/**
+	 * The extension's WP Plugin name.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string
+	 */
+	public $name = 'divi-cookie-consent';
+
+	/**
+	 * The extension's version
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string
+	 */
+	public $version = '1.0.0';
+
+	/**
+	 * DVPPL_DiviCookieConsent constructor.
+	 *
+	 * @param string $name
+	 * @param array  $args
+	 */
+	public function __construct( $name = 'divi-cookie-consent', $args = array() ) {
+		$this->plugin_dir     = plugin_dir_path( __FILE__ );
+		$this->plugin_dir_url = plugin_dir_url( $this->plugin_dir );
+
+		parent::__construct( $name, $args );
+	}
+}
+
+new DVPPL_DiviCookieConsent;
